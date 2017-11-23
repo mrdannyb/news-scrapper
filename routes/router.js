8d859162b6cfc05758105c577b8bccbd867f0312
mrdannyb <mr.dannyb@gmail.com>
2017-11-23 15:23:49 -0800

    set up view, working on routes and mongoose

--- routes/router.js
+++ routes/router.js
@@ -1,9 +1,46 @@
 const express = require('express');
 
+const db = require("./models");
+
 let router = express.Router();
 
 router.get('/', function (req,res) {
-  res.render('home');
+  db.Article
+    .find({})
+    .then(function (articles) {
+      res.render(articles);
+    })
+    .catch(function (err) {
+      res.json(err);
+    });
 });
+
+router.get('/scrape', function (req,res) {
+  //run a scrape into the db
+  //then read the db into the page
+  res.render('home')
+});
+
+router.get('/saved', function (req,res) {
+  res.render('home')
+})
+
+router.post('/save/:id',function (req,res) {
+  res.json(data)
+})
+
+router.post('/addComment/:id',function (req,res) {
+  //use mongoose to add the comment (req.body.comment)
+  //we will make
+  res.json(data)
+});
+
+router.delete('/delComment/:id', function (req,res) {
+  res.json(data)
+})
+
+router.delete('/delete/:id', function (req,res) {
+  res.json(data)
+})
 
 module.exports = router;
